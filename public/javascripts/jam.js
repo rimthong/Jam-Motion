694daa95a4a6a496246c0c6177d15c41980c58b4
Alexandre Rimthong <rimthong@gmail.com>
2014-05-24 17:05:57 -0400

    added fxs

--- public/javascripts/jam.js
+++ public/javascripts/jam.js
@@ -8,12 +8,14 @@ $(function() {
 
 	var x = 0, y = 0, z=0;
 
+  var instrument = 'kick'
+
 	function init() {
-		$("#hhat").on('click', function() { jam("hhat") });
-		$("#kick").on('click', function() { jam("kick") });
-		$("#snare").on('click', function() { jam("snare") });
-		$("#loop1").on('click', function() { jam("loop1") });
-		$("#loop2").on('click', function() { jam("loop2") });
+		$("#hhat").on('click', function() { instrument = 'hhat'; jam() });
+		$("#kick").on('click', function() { instrument = 'kick'; jam() });
+		$("#snare").on('click', function() { instrument = 'snare'; jam()});
+		$("#loop1").on('click', function() { instrument = 'loop1'; jam()});
+		$("#loop2").on('click', function() { instrument = 'loop2'; jam()});
 		$("#connect").unbind().on('click', connect);
 
 		initAccel();
@@ -44,63 +46,53 @@ $(function() {
 	}
 
 
-	
+  accelCalculation = function(e) {
+      e.preventDefault(); //disable shake undo
+
+      x = e.acceleration.x;
+      y = e.acceleration.y;
+      z = e.acceleration.z;
+
+      /*if ( e.rotationRate ) {
+        document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha;
+        document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta;
+        document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma;
+      }*/		
+
+      var absol = Math.sqrt(x*x + y*y + z*z);
+      
+      if (absol > 5) {
+        console.log(absol);
+      }
+    }
+
+	debouncedMessage = $.debounce( 250, accelCalculation)
+
 	function initAccel() {		
 		if (window.DeviceMotionEvent != undefined) {
-			window.ondevicemotion = function(e) {
-				e.preventDefault(); //disable shake undo
-
-				x = e.acceleration.x;
-				y = e.acceleration.y;
-				z = e.acceleration.z;
-
-				/*if ( e.rotationRate ) {
-					document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha;
-					document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta;
-					document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma;
-				}*/		
-			}
-
-			setInterval( function() {
-				var absol = Math.sqrt(x*x + y*y + z*z);
-				
-				if (absol > 5) {
-				console.log(absol);
-					$.debounce( 250, true, log_foo )
-					/*if (!isDebouncing) {
-						jam("snare");
-						sendAccel(x,y,z,absol); 
-						isDebouncing = true
-						debouncer = setTimeout(function() {isDebouncing = false}, 1000);
-					}*/
-				} else {
-					//isDebouncing = false;
-					//clearTimeout(debouncer);
-				}
-				
-				
-			}, 25);
+			window.ondevicemotion = debouncedMessage
 		} 
 	}
 
 function log_foo() {
 	console.log("foo");
 }
-	function sendAccel(x, y, z, absol) {
-		if (isAccelActive) {
-			//$("#accel").text(absol);
-			var accelObj = { "accel" : { "x":x, "y":y, "z":z , "abs": absol} }
-			//socket.emit('audio', accelObj);
-			console.log("sent accel " + accelObj)
-		}
-	}
 
-	function jam(msg) {
-		socket.emit('audio', { "instrument": msg });
-		console.log("sent audio " + msg)
-	}
-	
-	init(); 
+function sendAccel(x, y, z, absol) {
+  if (isAccelActive) {
+    //$("#accel").text(absol);
+    var accelObj = { "accel" : { "x":x, "y":y, "z":z , "abs": absol} }
+    //socket.emit('audio', accelObj);
+    console.log("sent accel " + accelObj)
+  }
+}
+
+function jam() {
+  socket.emit('audio', { "instrument": instrument });
+  console.log("sent audio " + instrument)
+}
+
+init(); 
 
 }
 )
